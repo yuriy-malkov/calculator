bccb899f22f6b3762da18df1f7baf47c098f372d
Yuriy Malkov <yuriy.malkov@outlook.com>
2020-01-18 11:43:21 -0500

    added multiplication and division; did some refactoring of test suite

--- src/test/scala/ca/malkov/yuriy/calculator/CalculatorSpec.scala
+++ src/test/scala/ca/malkov/yuriy/calculator/CalculatorSpec.scala
@@ -1,48 +1,62 @@
 package ca.malkov.yuriy.calculator
 
-import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.testkit.typed.scaladsl.{ActorTestKit, TestProbe}
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class CalculatorSpec
   extends AnyWordSpec
     with BeforeAndAfterAll
+    with BeforeAndAfterEach
     with Matchers {
 
   val testKit: ActorTestKit = ActorTestKit()
+  val probe: TestProbe[CalculatorSpec.Total] = testKit.createTestProbe[CalculatorSpec.Total]()
+  val calculator: ActorRef[CalculatorSpec.Command] = testKit.spawn(CalculatorSpec(0), "calculator")
 
   override def afterAll(): Unit = testKit.shutdownTestKit()
 
+  override def afterEach(): Unit = {
+    calculator ! CalculatorSpec.Reset()
+  }
+
   "Calculator" must {
-    val initialValue = 0
-    // create test probe (test actor) that expects a Total
-    val probe = testKit.createTestProbe[CalculatorSpec.Total]()
-    // Spawn calculator actor with initial value
-    val calculator = testKit.spawn(CalculatorSpec(initialValue), "calculator")
-    "add term to initial value" in {
-      val term = 1
-      val expectedValue = 1
-      calculator ! CalculatorSpec.Add(term)
+    val currentValue = 0
+    "addition: 0 + 1 = 1" in {
+      calculator ! CalculatorSpec.Add(1)
       calculator ! CalculatorSpec.Result(probe.ref)
-      probe.expectMessage(CalculatorSpec.Total(expectedValue))
+      probe.expectMessage(CalculatorSpec.Total(1))
     }
 
-    "subtract term from current value" in {
-      val term = 3
-      val expectedValue = -2
-      calculator ! CalculatorSpec.Subtract(term)
+    "subtraction: 5 - 3 = 2" in {
+      calculator ! CalculatorSpec.Add(5)
+      calculator ! CalculatorSpec.Subtract(3)
       calculator ! CalculatorSpec.Result(probe.ref)
-      probe.expectMessage(CalculatorSpec.Total(expectedValue))
+      probe.expectMessage(CalculatorSpec.Total(2))
     }
 
-    "reset value to 0" in {
+    "reset: 10 to 0" in {
       calculator ! CalculatorSpec.Add(10)
       calculator ! CalculatorSpec.Reset()
       calculator ! CalculatorSpec.Result(probe.ref)
       probe.expectMessage(CalculatorSpec.Total(0))
+    }
+
+    "multiplication: 2 * 3 = 6" in {
+      calculator ! CalculatorSpec.Add(2)
+      calculator ! CalculatorSpec.Multiply(3)
+      calculator ! CalculatorSpec.Result(probe.ref)
+      probe.expectMessage(CalculatorSpec.Total(6))
+    }
+
+    "division: 9 / 3 = 3" in {
+      calculator ! CalculatorSpec.Add(9)
+      calculator ! CalculatorSpec.Divide(3)
+      calculator ! CalculatorSpec.Result(probe.ref)
+      probe.expectMessage(CalculatorSpec.Total(3))
     }
   }
 }
@@ -52,6 +66,8 @@ object CalculatorSpec {
   sealed trait Command
   final case class Add(term: Int) extends Command
   final case class Subtract(term: Int) extends Command
+  final case class Multiply(term: Int) extends Command
+  final case class Divide(term: Int) extends Command
   final case class Reset() extends Command
   final case class Result(replyTo: ActorRef[Total]) extends Command
   final case class Total(n: Int)
@@ -66,6 +82,10 @@ object CalculatorSpec {
           calculator(total + term)
         case Subtract(term) =>
           calculator(total - term)
+        case Multiply(term) =>
+          calculator(total * term)
+        case Divide(term) =>
+          calculator(total / term)
         case Reset() =>
           calculator(0)
         case Result(replyTo) =>
